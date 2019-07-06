04a796c300f81544f19fc4e9ee622318ff8c87d1
Dorris Wong <pingki@sbcglobal.net>
2019-07-06 01:57:14 -0700

    Send API image response to S3

---
+++ helper/uploadS3.js
@@ -0,0 +1,31 @@
+const AWS = require('aws-sdk');
+const config = require('../config.js');
+const request = require('request');
+
+AWS.config.update({accessKeyId: `${config.AWSAccessKeyId}`, secretAccessKey: `${config.AWSSecretKey}`});
+
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+   
+function put_from_url(callback) {
+    request({
+        url: 'https://images.pexels.com/photos/1268558/pexels-photo-1268558.jpeg',
+        encoding: null
+    }, function(err, res, body) {
+        if (err)
+            return callback(err, res);
+
+        s3.putObject({
+            Bucket: 'reversly-photos',
+            Key: 'test.jpg',
+            ContentType: res.headers['content-type'],
+            Body: body 
+        }, callback);
+    })
+}
+   
+put_from_url(function(err, res) {
+    if (err)
+        throw err;
+
+    console.log('Uploaded data successfully!');
+});
