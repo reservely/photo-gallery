24fdd1cb115c84ebefde463d5b9e48eee96d6b1a
Dorris Wong <pingki@sbcglobal.net>
2019-07-06 22:13:34 -0700

    Connect server to mongodb

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const db = require('../db/image.js');
 
 const app = express();
 const port = 3000; 
 
 app.use(morgan('dev'));
+app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/images', (req, res) => {
+  db.find((err, data) => {
+    if (err) console.log('error');
+    res.send(data);
+  }).limit(10)
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
