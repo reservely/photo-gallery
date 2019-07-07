f9e5915af4c8b6103f3cb9502527f1a0e1ebd1a2
Dorris Wong <pingki@sbcglobal.net>
2019-07-06 23:21:26 -0700

    Fix frontend dev environment

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
-ReactDom.render(<div>Hello World</div>, document.getElementById('app'));
+ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('app'));
