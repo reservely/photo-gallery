ba2393bdf67ae987867985e8396061720e63a611
Dorris Wong <pingki@sbcglobal.net>
2019-07-06 23:56:13 -0700

    Create App component

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+
+  render(){
+    return <h1>Hello</h1>;
+  }
+}
+
+export default App;
