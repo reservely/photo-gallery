959dd206bc4bdb4d44c0ff7d776a25e0fef4eae5
Dorris Wong <pingki@sbcglobal.net>
2019-07-11 21:30:04 -0700

    Implement left and right buttons

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from '../styles/Grid.css';
 
-const Modal = ({image, handleImageClick}) => (
-  <div className={styles.modal}>
+const Modal = ({image, handleImageClick}) => {
+  const left = '<';
+  const right = '>';
+
+  return (<div className={styles.modal}>
     <span className={styles.close} onClick={() => handleImageClick(event)}>X</span>
-    <img className={styles.modalContent} src={image}  height='124.45' width='124.45' />
-  </div>
-);
+    <div className={styles.smallContainer}>
+      <span className={styles.left}>{left}</span>
+      <img className={styles.modalContent} src={image}  height='124.45' width='124.45' />
+      <span className={styles.right}>{right}</span>
+    </div>
+  </div>)
+};
 
 export default Modal;
