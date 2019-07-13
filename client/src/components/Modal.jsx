5aac606c8eed4bf7c8867f6b88ce61d8ee47406f
Dorris Wong <pingki@sbcglobal.net>
2019-07-12 20:24:08 -0700

    Auto resize photos and align arrows next to the picture

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -8,9 +8,11 @@ const Modal = ({handleKeyPress, onClickBack, onClickForward, imgURL, index, hand
   return (
     <div className={styles.modal}>
       <span className={styles.close} onClick={() => handleImageClick(event)}>X</span>
-      <span className={styles.left} onClick={() => onClickBack()}>{left}</span>
-      <img className={styles.modalContent} src={imgURL[index]}  height='124.45' width='124.45' onKeyDown = {()=>handleKeyPress(e)}/>
-      <span className={styles.right} onClick={() => onClickForward()}>{right}</span>
+      <div className={styles.outsideContainer}>
+        <span className={styles.left} onClick={() => onClickBack()}>{left}</span>
+        <img className={styles.modalContent} src={imgURL[index]}  height='124.45' width='124.45' onKeyDown = {()=>handleKeyPress(e)}/>
+        <span className={styles.right} onClick={() => onClickForward()}>{right}</span>
+      </div>
     </div>
   )
 };
