fa2eda3410cfb3bf7544ff34d3a46932f0fa370b
Dorris Wong <pingki@sbcglobal.net>
2019-07-15 09:12:57 -0700

    Apply 1 test

--- test/Gallery.test.js
+++ test/Gallery.test.js
@@ -1,8 +1,35 @@
 import ReactDOM from 'react-dom';
 import Gallery from '../client/src/components/Gallery.jsx';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Gallery />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Gallery rendering', () => {
+  let wrapper = shallow(<Gallery />);
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Gallery />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should render Grid child component', () => {
+    expect(wrapper.find(Grid)).toHaveLength(1);
+  });
+});
+
+describe('Grid Component', () => {
+
+  beforeEach(() => {
+    const photos = ['http://www.google.com',
+    'http://www.google.com',
+    'http://www.google.com',
+    'http://www.google.com',
+    'http://www.google.com',
+    'http://www.google.com',
+    'http://www.google.com',
+    'http://www.google.com',
+    'http://www.google.com'
+    ];
+    const wrapper = shallow(<Gallery photos={photos}/>);
+
+    expect(wrapper).toMatchSnapshot();
+  });
 });
